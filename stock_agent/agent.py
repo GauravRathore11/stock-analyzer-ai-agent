2000f9ac9065fcbaa3094a6aa7a98f4d594fddcd
Gaurav Rathore <gauravrathore856@gmail.com>
2026-04-20 18:27:23 +0530

    added local ollama model for testing

--- stock_agent/agent.py
+++ stock_agent/agent.py
@@ -1,9 +1,12 @@
 from google.adk.agents.llm_agent import Agent
 from stock_agent.prompts import system_instruction, description
 from stock_agent.tools import get_stock_price, get_historical_data, get_stock_fundamentals
+from google.adk.models.lite_llm import LiteLlm
+
+local_model = LiteLlm(model="ollama_chat/qwen2.5:0.5b")
 
 root_agent = Agent(
-    model = 'gemini-2.5-flash',
+    model = local_model,
     name = 'root_agent',
     description = description,
     instruction = system_instruction,
